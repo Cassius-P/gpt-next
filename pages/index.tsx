41bb68308a2ba8d0d7fe1ed7bc65c6fe6631a60b
Ilias <ilias.kelesoglu@globule.net>
2023-04-05 23:48:52 +0200

    Display messages on new chat

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,23 @@
 import Container from "@/components/container/Container";
 import Sidebar from "@/components/sidebar/Sidebar";
+import {useConversation} from "@/components/utils/ConversationContext";
+import {Message} from "@/models/Message";
+import Highlighter from "@/components/container/Highlighter";
+import MessageUI from "@/components/container/Message";
 
 export default function Home() {
+
+    const { activeConversationMessages  } = useConversation()
+
     return (
-      <div className="flex h-full">
-        
-      </div>
+        <div className="flex h-full flex-col spacing-2" id="chat-container">
+            {activeConversationMessages.map((message:Message, index:string) => {
+                return (
+                    <MessageUI message={message} key={index}/>
+                )
+            })
+            }
+        </div>
     )
-  }
+
+}
