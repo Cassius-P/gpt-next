bb3df4a13de1e2f6072fcd52e20de5580d409217
Ilias <ilias.kelesoglu@globule.net>
2023-04-06 00:07:31 +0200

    Keyboard ShortCuts mapper

---
+++ components/utils/KeyboardHandler.tsx
@@ -0,0 +1,67 @@
+import {createContext, ReactNode, useContext, useEffect} from "react";
+import Mousetrap from "mousetrap"
+import {useUI} from "@/components/UIContext";
+
+export const KeyboardContext = createContext<any>({})
+KeyboardContext.displayName = "KeyboardContext";
+
+
+interface ShortCut {
+    keys: string | string[],
+    callback: () => void
+    action?: "keypress" | "keydown" | "keyup"
+
+}
+
+
+
+
+
+export const KeyboardHandler = ({ children } : {children:ReactNode}) => {
+    const {openModal, closeModal, setModalView, displayModal} = useUI();
+
+    const toggleSearch: ShortCut = {
+        keys: "alt+k",
+        callback: async () => {
+            let action = "Open"
+            if(!displayModal) {
+                openModal();
+                setModalView("SEARCH_VIEW")
+            }else {
+                action = "Close"
+                closeModal();
+            }
+
+            console.log(`${action} Search Modal`)
+        }
+    }
+
+    Mousetrap.bind(toggleSearch.keys, toggleSearch.callback, toggleSearch.action)
+
+
+
+
+    const shortcuts: ShortCut[] = [
+        toggleSearch
+    ]
+
+    useEffect(() => {
+
+        for (const argument of shortcuts) {
+            if(!argument.action) argument.action = "keypress"
+
+        }
+
+
+    }, [displayModal])
+
+
+    const removeBinds = (shortcut:ShortCut) => {
+        Mousetrap.unbind(shortcut.keys)
+    }
+
+
+    return <KeyboardContext.Provider value={{removeBinds, toggleSearch}}>{children}</KeyboardContext.Provider>
+}
+
+export const useKeyboard = () => useContext(KeyboardContext);
